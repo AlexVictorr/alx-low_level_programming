530f7677521affaa88a5c086f7c02d0c82c726ae
Abiakam Victor <vvabiakam@gmail.com>
2023-02-16 15:12:04 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #!/bin/bash
 #include<stdio.h>
-int main(){
-	char str[] = "Programming is like building a multilingual puzzle";
-	puts(str);
-	return(0);
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char string[] = "Programming is like building a multilingual puzzle";
+	puts(string);
+	return 0;
 }
