430b894bb67a6484a8624d5fb72d5f9dc62ad972
Abiakam Victor <vvabiakam@gmail.com>
2023-02-17 17:21:30 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints all single digits of base 10 starting from 0
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char num[10] = "0123456789";
+	int i;
+
+	for (i = 0; i <= 10)
+	{
+		putchar(num[i]);
+	}
+	putchar('\n');
+	return (0);
+}
