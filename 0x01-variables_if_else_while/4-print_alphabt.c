dce8fff9b2297fe73cb60f20824abd802fd10f94
Abiakam Victor <vvabiakam@gmail.com>
2023-02-17 16:59:22 -0800

    When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-	char alpha[25] = "abcdefghijklmnopqrstuvwxyz";
+	char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
-	for (i = 0; i <= 25; i++)
+	for (i = 0; i < 26; i++)
 	{
 		if (i != 4 && i != 16)
 		{
