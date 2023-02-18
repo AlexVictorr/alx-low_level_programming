00c803ee842d8654ee8568237936ae97f17726bd
Abiakam Victor <vvabiakam@gmail.com>
2023-02-17 16:06:12 -0800

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,10 +8,10 @@ int main(void)
 	char alpha[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
-	for(i = 0; i <= 52; i++)
+	for(i = 0; i < 52; i++)
 	{
 		putchar(alpha[i]);
 	}
-	putchar("\n);
+	putchar("\n");
 	return (0);
 }
