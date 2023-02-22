3188579db03c21dfb97e9ee92856c7fae041d2c4
Abiakam Victor <vvabiakam@gmail.com>
2023-02-21 20:55:30 -0800

    Nature made the natural numbers; All else is the work of women

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_times_table - prints the sum of all multiples of 3 or 5 up to 1024
+ * Return: Always (success)
+ */
+int main(void)
+{
+	int i, z = 0;
+
+	while (i < 1024)
+	{
+		if ((i % 3 == 0) || (i % 5 = 0))
+		{
+			z += i;
+		}
+		i++;
+	}
+	_putchar(z + '0');
+	return (0);
+}
