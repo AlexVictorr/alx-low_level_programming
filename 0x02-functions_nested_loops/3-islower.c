ba3bcbe108ec014880c5e13aa7eb5668e91e50c4
Abiakam Victor <vvabiakam@gmail.com>
2023-02-21 17:41:30 -0800

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower - checks for lowercase character
+ * Return: 1 (success0
+ */
+int _islower(int c)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z')
+	{
+		_putchar(l);
+	}
+	return (1);
+}
