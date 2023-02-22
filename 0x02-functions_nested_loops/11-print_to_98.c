9ed2e179c600cad41a171acd7374f2fc17b7ab99
Abiakam Victor <vvabiakam@gmail.com>
2023-02-21 20:22:29 -0800

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: natural number to start from
+ * Return: Always 0 (success)
+ */
+void print_to_98(int n)
+{
+	for (; n <= 98; n++)
+	{
+		if (n == 98)
+		{
+			_putchar(n);
+			_putchar('\n');
+			break;
+		}
+		else
+		{
+			_putchar(n);
+			_putchar(44);
+			_putchar(32);
+		}
+	}
+	for (; n >= 98; n--)
+	{
+		if (n == 98)
+		{
+			_putchar(n);
+			_putchar('\n');
+			break;
+		}
+		else
+		{
+			_putchar(n);
+			_putchar(44);
+			_putchar(32);
+		}
+	}
+}
