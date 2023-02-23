0b8b9e818e043857fa0262279e01114f17ecd6eb
Abiakam Victor <vvabiakam@gmail.com>
2023-02-23 02:01:26 -0800

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase character
+ * @c: character to be checked
+ * Return: 1 for uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	for (c = 65; c <= 90; c++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (1);
+}
