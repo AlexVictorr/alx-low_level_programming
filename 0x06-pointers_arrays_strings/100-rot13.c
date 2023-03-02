05ff1d78f19de02a7e2866c417a55c4882f64cec
Abiakam Victor <vvabiakam@gmail.com>
2023-03-02 15:47:49 -0800

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rot13 - encodes a string using rot13.
+ * @s: pointer to string to be computed.
+ * Return: pointer to s.
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char rot1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == rot1[j])
+			{
+				s[i] = rot2[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
